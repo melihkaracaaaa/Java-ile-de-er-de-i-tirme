aab65dca84389ec5916edf30fe052187026bd24a
Melihcan Karaca <karacamelihcan1@gmail.com>
2022-09-05 21:12:10 +0300

    Update IkıSayınınDegerınıDegıstırme.java

--- IkıSayınınDegerınıDegıstırme/src/IkıSayınınDegerınıDegıstırme.java
+++ IkıSayınınDegerınıDegıstırme/src/IkıSayınınDegerınıDegıstırme.java
@@ -2,29 +2,29 @@
 import java.util.Scanner;
 
 
-public class IkıSayınınDegerınıDegıstırme {
+public class İkiSayininDegeriniDegistirme {
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     
     int birinci_sayi;
     int ikinci_sayi;
     
-    System.out.print("Birinci sayı :");
+    System.out.print("Birinci sayi :");
     birinci_sayi = scanner.nextInt();
     
-    System.out.print("İkinci sayı :");
+    System.out.print("İkinci sayi :");
     ikinci_sayi = scanner.nextInt();
-    System.out.println("Değiştirilmeden önce...");
+    System.out.println("Degistirilmeden once...");
         
-    System.out.println("Birinci sayı :" + birinci_sayi + "  --  " + "ikinci sayı :" + ikinci_sayi);
+    System.out.println("Birinci sayi :" + birinci_sayi + "  --  " + "ikinci sayi :" + ikinci_sayi);
     
     
     int gecici = birinci_sayi;
     birinci_sayi = ikinci_sayi;
     ikinci_sayi = gecici;
     
-    System.out.println("Değiştirildikten sonra :");
-    System.out.println("Birinci sayı :" + birinci_sayi + "  --  " + "ikinci sayı :" + ikinci_sayi);
+    System.out.println("Degistirildikten sonra :");
+    System.out.println("Birinci sayi :" + birinci_sayi + "  --  " + "ikinci sayi :" + ikinci_sayi);
 
         
     }
